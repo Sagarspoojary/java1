fd2b71e020d2777df567aff8e19e10049d1ed37d
Sagarspoojary <sagar.23cs125@sode-edu.in>
2024-09-26 10:14:58 +0530

    Update K90.java

--- K90.java
+++ K90.java
@@ -1,18 +1,18 @@
-class Bank2{                    // parent class
+class Bank2{                  
     int getInterestRate(){
     return 0;}}
 class Canara extends Bank2{
     int getInterestRate(){
         return 5;}}
-class SBI extends Bank2{        // child class
-    int getInterestRate(){      // method overriding
+class SBI extends Bank2{       
+    int getInterestRate(){    
         return 6;}}
 class SCDCC extends Bank2{
     int getInterestRate(){
         return 8;}}
 public class K90{
     public static void main(String[] args){
-        Canara bank1=new Canara();    //object created
+        Canara bank1=new Canara();   
         SBI bank2 = new SBI();
         SCDCC bank3 = new SCDCC();
         System.out.println("Bank1: " + bank1.getInterestRate() + " %");
